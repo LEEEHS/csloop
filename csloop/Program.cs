51bee17eb979a7f313370ca5b9661b5175d5c642
ehsyb <ehsyb@naver.com>
2020-05-11 08:40:27 +0900

    초기 커밋.

---
+++ csloop/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace csloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*long start = DateTime.Now.Ticks;
+            long count = 0;
+            while (start + (10000000) > DateTime.Now.Ticks){
+                count++;
+            }
+            Console.Write(count + "만큼 반복함");
+            string[] array = { "사과", "배", "포도", "바나나"};
+            foreach (string item in array)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+    }*/
+            int x = 1;
+            while (x < 50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+                if (x % 3 == 0)
+                    Console.WriteLine("__@");
+                else if (x % 3 == 1)
+                    Console.WriteLine("_^@");
+                else
+                    Console.WriteLine("^_@");
+                Thread.Sleep(100);
+                x++;
+            }
+        }
+    }
+}
